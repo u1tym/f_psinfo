68c524428e3bb818c072fdab4d027bd825f56d82
u1 <y.tym@me.com>
2026-05-03 17:58:44 +0900

    path修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const apiOrigin = env.VITE_PSINFO_ORIGIN || 'http://127.0.0.1:8000'
 
   return {
+    base: '/mobile/psinfo/',
     plugins: [vue()],
     server: {
       proxy: {
